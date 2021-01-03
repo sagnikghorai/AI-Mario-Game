3a756439721ac5596e55b4eeab46e27bdc732618
sagnikghorai <63553430+sagnikghorai@users.noreply.github.com>
2021-01-03 19:58:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function preload() {
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
@@ -7,33 +12,34 @@ function preload() {
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
-	
-	instializeInSetup(mario);
 
+	instializeInSetup(mario);
+	
 	video = createCapture(VIDEO);
-	video.size(800, 400);
+	video.size(800,400);
 	video.parent('game_console');
 
 	poseNet = ml5.poseNet(video, modelLoaded);
-	poseNet.on('pose' , gotPoses);
+	poseNet.on('pose', gotPoses);
+
 }
 
 function modelLoaded() {
 	console.log('Model Loaded!');
-}
+  }
 
-function gotPoses(results)
-{
+  function gotPoses(results)
+  {
 	if(results.length > 0)
 	{
-		console.log(results);
-		noseX = results[0].pose.nose.x;
-		noseY = results[0].pose.nose.y;
-		console.log("noseX = " + noseX +", noseY = " + noseY);
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
 	}
-}
+  }
+   
 function draw() {
-	game()
+	game();
 }
 
 
